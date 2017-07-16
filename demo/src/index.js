7619bb735292f5b077011e1e5e8edceaa462f14e
Felipe N. Moura <felipenmoura@gmail.com>
2017-07-15 23:37:56 -0300

    Adding files

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,133 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
 
-import Example from '../../src'
+import { HermesComponent } from '../../src'
+require('../../css/demo.scss')
 
 class Demo extends Component {
+  constructor (props) {
+    super(props)
+
+    this.getNextMessage = 
+    this.messages = {
+      default: [
+        'A defaule message here',
+        'Coding is cool, right?',
+        'Just another message',
+        'Yet another useless message'
+      ],
+      warn: [
+        'A warn message here',
+        'Warns make me worried!',
+        'Don\'t worry, this is just a warning',
+        'Warning...warning...warning...',
+        'You have been warned!'
+      ],
+      info: [
+        'An info message here',
+        'Info messages, wohoo!!',
+        'Don\'t worry, this is just an info',
+        'Make yourself informed with this!'
+      ],
+      error: [
+        'OMG OMG OMG!',
+        'DANGER!',
+        'Something is out of order!',
+        'Sorry...I got no idea!'
+      ]
+    }
+
+    this.showRandomMessage = this.showRandomMessage.bind(this)
+    this.showABunchOfMessages = this.showABunchOfMessages.bind(this)
+  }
+  showRandomMessage (data = {}) {
+    data.type = data.type || 'default'
+    let msg = this.messages[data.type]
+    msg = msg[Math.ceil(Math.random() * (msg.length - 1))]
+    data.body = msg
+    Hermes.message(data)
+  }
+  showABunchOfMessages () {
+    Hermes.message({body: 'Some random message'})
+    Hermes.message({body: 'Ouch, something went wrong!', type: 'error'})
+    Hermes.message({body: 'Another rather ordinary message!'})
+    Hermes.message({body: 'Also this, for 3 seconds', type: 'info', duration: 3})
+    Hermes.message({body: 'A warning message', type: 'warn'})
+    Hermes.message({body: 'Another useless message', type: 'success'})
+  }
+
+  showSavingMessage () {
+    Hermes.message({
+      id: 'savingMessage',
+      body: 'Saving...',
+      locked: true,
+      animate: true
+    })
+    
+    setTimeout(_ => {
+      Hermes.updateMessage({
+        id: 'savingMessage',
+        body: 'Saved',
+        type: 'success',
+        locked: true,
+        duration: 3
+      })
+    }, 3000)
+  }
+
+  showReactMessage () {
+    Hermes.message({
+      id: 'demoWithReact',
+      body: <div className='react-example'>
+        We could use other elements here:<br />
+        <input type="text" placeholder='say something...' /><br />
+        <textarea placeholder='why did you say that?' />
+        <input type="button" value='ok' onClick={event => Hermes.closeMessage('demoWithReact')} />
+      </div>
+    })
+  }
+
   render() {
     return <div>
-      <h1>react-name-surname Demo</h1>
-      <Example/>
+      <HermesComponent defaultDuration='8' updateTitleCounter />
+
+      <h3>
+        <div className='hermes-header' />
+      </h3>
+
+      <div className='example-container'>
+        Just a message
+        <div className='trigger-btn' type='button' onClick={event=>{this.showRandomMessage({type: 'default'})}} >►</div>
+      </div>
+      <div className='example-container'>
+        A warning
+        <div className='trigger-btn' type='button' onClick={event=>{this.showRandomMessage({type: 'warn'})}} >►</div>
+      </div>
+      <div className='example-container'>
+        An error
+        <div className='trigger-btn' type='button' onClick={event=>{this.showRandomMessage({type: 'error'})}} >►</div>
+      </div>
+      <div className='example-container'>
+        An info
+        <div className='trigger-btn' type='button' onClick={event=>{this.showRandomMessage({type: 'info'})}} >►</div>
+      </div>
+      <div className='example-container'>
+        A locked message
+        <div className='trigger-btn' type='button' onClick={event=>{Hermes.closeMessage('lockedMessage')}} >Hide</div>
+        <div className='trigger-btn' type='button' onClick={event=>{this.showRandomMessage({locked: true, id: 'lockedMessage'})}} >Show</div>
+      </div>
+      <div className='example-container'>
+        A bunch of messages
+        <div className='trigger-btn' type='button' onClick={event=>{this.showABunchOfMessages()}} >►</div>
+      </div>
+      <div className='example-container'>
+        Updating a message after a few seconds
+        <div className='trigger-btn' type='button' onClick={event=>{this.showSavingMessage()}}> ►</div>
+      </div>
+      <div className='example-container'>
+        Show message with a react component in it
+        <div className='trigger-btn' type='button' onClick={event=>{this.showReactMessage()}}> ►</div>
+      </div>
     </div>
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,268 @@
 import React, {Component} from 'react'
+require('../css/default.scss')
 
-export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
+function* idsGen () {
+  let i = 0
+  while(true) {
+    i++
+    yield i
+  }
+}
+const idGen = idsGen()
+
+export const Hermes = {
+  mountedInstance: null,
+  message: (content, data) => {
+    if (!data) {
+      data = content
+    } else {
+      data.body = content
+    }
+    if (global.Hermes.mountedInstance) {
+      if (typeof data === 'string') {
+        data = {
+          body: data,
+          duration: 10
+        }
+      }
+      global.Hermes.mountedInstance.addMessage(data)
+    }
+  },
+  updateMessage: (data) => {
+    if (global.Hermes.mountedInstance) {
+      global.Hermes.mountedInstance.updateMessage(data.id, data)
+    }
+  },
+  closeMessage (messageId) {
+    if (global.Hermes.mountedInstance) {
+      global.Hermes.mountedInstance.removeMessage(messageId)
+    }
+  },
+  warn (message, options = {}) {
+    options.type = 'warn'
+    global.Hermes.message(message, options)
+  },
+  warning (message) {
+    options.type = 'warn'
+    global.Hermes.message(message, options)
+  },
+  error (message) {
+    options.type = 'error'
+    global.Hermes.message(message, options)
+  },
+  fail (message) {
+    options.type = 'error'
+    global.Hermes.message(message, options)
+  },
+  info (message) {
+    options.type = 'info'
+    global.Hermes.message(message, options)
+  },
+  success (message) {
+    options.type = 'success'
+    global.Hermes.message(message, options)
+  }
+}
+
+global.Hermes = Hermes
+
+export class HermesComponent extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      messages: [],
+      originalTitle: document.title
+    }
+  }
+
+  setMessageDuration (data) {
+    let duration = data.duration || (data.locked ? null : this.props.defaultDuration)
+    if (duration) {
+      let s = Math.max(1, duration) * 1000
+      data.to = setTimeout(_ => {
+        this.removeMessage(data)
+      }, s)
+    }
+    return data
+  }
+
+  updateTitleCounter () {
+    if (this.props.updateTitleCounter) {
+      let counter = this.state.messages.filter(_=>_).length
+      if (document.title) {
+        if (counter) {
+          document.title = '(' + counter + ') ' + this.state.originalTitle
+        } else {
+          document.title = this.state.originalTitle
+        }
+      }
+    }
+  }
+
+  addMessage (data) {
+    if (!Object.isExtensible(data)) {
+      // this is probably a react component
+      data = {
+        body: data
+      }
+    }
+    data.id = data.id || idGen.next().value
+    let list = this.state.messages
+    list[data.id] = data
+
+    data = this.setMessageDuration(data)
+    // if (duration) {
+    //   let s = duration * 1000
+    //   data.to = setTimeout(_ => {
+    //     this.removeMessage(data)
+    //   }, s)
+    // }
+
+    this.setState({
+      messages: list
+    }, _ => {
+      this.updateTitleCounter()
+      data = this.setMessageDuration(data)
+      if (data.onShow) {
+        data.onShow(data)
+      }
+    })
+  }
+
+  updateMessage (id, data) {
+    let list = this.state.messages
+    if (id && list[id]) {
+      let message = list[id]
+      message.body = data.body
+      message.type = data.type
+      if (data.duration) {
+        if (message.to) {
+          clearTimeout(message.to)
+        }
+        message = this.setMessageDuration(data)
+      }
+      list[id] = message
+      this.setState({
+        messages: list
+      })
+    } else {
+      data.id = id || data.id || idGen.next().value
+      this.addMessage(data)
+    }
+  }
+
+  removeMessageElement (data) {
+    let id = data && data.id ? data.id : data
+    let list = this.state.messages
+    delete list[id]
+    this.setState({
+      messages: list
+    }, _ => {
+      this.updateTitleCounter()
+      if (data.to) {
+        clearTimeout(data.to)
+      }
+    })
+  }
+
+  removeMessage (data) {
+    let id = data && data.id ? data.id : data
+    let list = this.state.messages
+    if (list[id]) {
+      list[id].removing = true
+      this.setState({
+        messages: list
+      }, _ => {
+        setTimeout(_ => {
+          this.removeMessageElement(data)
+        }, 1000)
+      })
+    }
+  }
+
+  componentDidMount () {
+    global.Hermes.mountedInstance = this
+  }
+
+  render () {
+    let notifs = Object.keys(this.state.messages)
+    return <div id='hermes-container'>
+      {
+        notifs.map((messageId, i) => {
+          let message = this.state.messages[messageId]
+          let order = (notifs.length - i)
+          return message
+            ? <div
+              key={message.id}
+              className={`hermes-notif ${message.animate ? 'animate' : ''} hermes-${message.type || 'default'} ${(message.removing ? 'removing' : '')}`}
+              style={message.removing ? { order } : {
+                animationDelay: (i * 0.1) + 's',
+                order
+              }}>
+              {message.body}
+              {
+                message.locked && message.id
+                  ? null
+                  : <span
+                    className='hermes-remove-message'
+                    onClick={event => this.removeMessage(message)}>x</span>
+              }
+            </div>
+            : null
+        })
+      }
     </div>
   }
 }
+
+export default global.Hermes
+
+// These global methods below, are o help you in case you was already using
+// Bert's show[info/error/warning/success]
+export const showInfo = (message) => {
+  window.Hermes.updateMessage({
+    body: message,
+    duration: 10,
+    type: 'info'
+  })
+}
+
+export const showError = (message) => {
+  window.Hermes.updateMessage({
+    body: message,
+    duration: 15,
+    id: message,
+    type: 'error'
+  })
+}
+
+export const showWarning = (message) => {
+  window.Hermes.updateMessage({
+    body: message,
+    duration: 10,
+    type: 'warn'
+  })
+}
+
+export const showSuccess = (message) => {
+  window.Hermes.updateMessage({
+    body: message,
+    duration: 4,
+    id: 'success',
+    type: 'default'
+  })
+}
+
+
+/*  SAMPLES
+window.Hermes.message({body: 'Outra mensagem', type: 'error'})
+window.Hermes.message({body: 'Uma mensagem aqui'})
+window.Hermes.message({body: 'Also this', type: 'info', duration: 3})
+window.Hermes.message({body: 'Oh my gosh!', type: 'warn'})
+window.Hermes.message({body: 'Mais uma mensavem inútil aqui :)'})
+window.Hermes.updateMessage({body: 'Saving...', locked: true, id: 'savingMSG'})
+setTimeout(function () {
+  window.Hermes.updateMessage({body: 'Saved', locked: true, id: 'savingMSG', duration: 3})
+}, 6000)
+*/
